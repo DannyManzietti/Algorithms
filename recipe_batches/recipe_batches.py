de4a3cd76e62c003712d1764c2023e328fd59d1c
Danny Manzietti <dannymanzietti@Dannys-MacBook-Pro.local>
2020-02-19 17:56:41 -0500

    recipe batches complete and test completes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,27 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    max_batches = float('inf')
+
+    for i in recipe:
+        if ingredients.get(i) is None:
+            return 0
+        if recipe[i] > ingredients[i]:
+            return 0
+
+        current_batches = ingredients[i] // recipe[i]
+        if current_batches < max_batches:
+            max_batches = current_batches
+
+    return max_batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
